7653b9950c117f6c4683d4e1c7d9dd44dc082087
pivotal-kahin-ng <kang@pivotal.io>
2017-10-01 17:32:07 -0400

    Set up

---
+++ ketch-up-UITests/LandingViewControllerUISpec.swift
@@ -0,0 +1,14 @@
+import XCTest
+import EarlGrey
+
+class LandingViewControllerUISpec: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+}
---
+++ ketch-up-tests/LandingViewControllerSpec.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+
+//@testable import LandingViewController
+
+class LandViewControllerSpec: QuickSpec {
+    override func spec() {
+        beforeEach {
+            
+        }
+        
+    }
+}
